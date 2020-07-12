81dba7ae295901fba5d85f71b0a05d1d2a962c37
Stacy <yolanda7zhangwoo@gmail.com>
2020-07-12 17:17:04 -0400

    update mailmerge

--- mailmerge/mailmerge.sh
+++ mailmerge/mailmerge.sh
@@ -5,3 +5,28 @@
 #
 #  Created by Stacy Zhang on 7/11/20.
 #  
+
+numOfLines=$(awk 'END {print NR}' secondary)
+
+
+for ((i=1; i<=$numOfLines;++i))
+do
+
+    first=$(awk -F':' -v var="$i" 'NR==var {print $1}' secondary)
+    #echo $first
+    second=$(awk -F':' -v var="$i" 'NR==var {print $2}' secondary)
+    #echo $second
+    third=$(awk -F':' -v var="$i" 'NR==var {print $3}' secondary)
+    #echo $third
+    fourth=$(awk -F':' -v var="$i" 'NR==var {print $4}' secondary)
+    #echo $fourth
+    echo ""
+    echo ""
+    cat primary|sed "s|#1|$first|g;s|#2|$second|g;s|#3|$third|g;s|#4|$fourth|g"
+    echo ""
+    echo ""
+    
+done
+
+
+
